4b3f3f70981550e438fd0ff325e67b12247652c7
hahalol47 <idriss.maaroufi@hotmail.fr>
2020-02-22 17:05:51 +0100

    first commmmit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import image from "./335915.png"
+import "./style.css"
+
+function App() {
+  return (
+
+<div style={{ border:'solid 1px black', maxWidth:'100vw'}}>
+
+<h1 className='title red' > My title </h1>
+
+<br/>
+
+<img src={image} />
+<img src="/61082.jpg" />
+
+</div> )}
+
+export default App;
